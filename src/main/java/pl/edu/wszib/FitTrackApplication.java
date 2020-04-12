1c3ea2e4e34b55ccab1849ea5b88b8f9187d34f3
tgugula <tgugula@wszib.edu.pl>
2020-04-12 18:58:19 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/FitTrackApplication.java
@@ -0,0 +1,11 @@
+package pl.edu.wszib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FitTrackApplication.class, args);
+    }
+}
